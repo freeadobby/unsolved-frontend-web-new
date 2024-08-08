1bd83519eff1de5a8c5601db22e9a3053691aab4
giyukim <giyu1020@gmail.com>
2024-08-08 22:19:45 +0900

    initial

---
+++ src/main/java/kr/gyk/adobby/unsolved_frontend/UnsolvedFrontendApplication.java
@@ -0,0 +1,13 @@
+package kr.gyk.adobby.unsolved_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsolvedFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnsolvedFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/gyk/adobby/unsolved_frontend/UnsolvedFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gyk.adobby.unsolved_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsolvedFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
